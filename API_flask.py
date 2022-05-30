90c299c5bc68413b43c0377be58529fc249f487c
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-30 11:38:16 -0300

    projeto finalizado com um formulário html e autenticação de login e senha retornando o token ou um erro

--- API_flask.py
+++ API_flask.py
@@ -1,20 +1,31 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, render_template, request
 import jwt
-
-login = input("Login: ")
-password = input("Password: ")
-login_pass = "aluno"
-password_pass = "easycredito"
 
 app = Flask(__name__)
 
-@app.route("/teste",methods=['GET'])
-
+@app.route("/",methods=['GET', 'POST'])
 def index():
-    encoded_jwt = jwt.encode({'token': ''}, "secret", algorithm='HS256')
-    jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
-    if login == login_pass and password == password_pass:
-        return jsonify({'token': encoded_jwt})
+    if request.method == 'GET':
+        # importando arquivo html
+        return render_template("index.html")
+    else:
+        # pegando login e senha do formulário
+        login = request.form.get("login")
+        password = request.form.get("password")
+
+        # login e senha corretos
+        login_pass = "aluno"
+        password_pass = "easycredito"
+
+        # autenticando login e senha
+        if login == login_pass and password == password_pass:
+            # gerando token
+            encoded_jwt = jwt.encode({'token': ''}, "secret", algorithm='HS256')
+            jwt.decode(encoded_jwt, "secret", algorithms=["HS256"])
+            token = jsonify({'token': encoded_jwt})
+            return token
+        else:
+            # retornando erro de senha ou login
+            return '<h1>Login ou senha inválido</h1>'
 if __name__ == "__main__":
     app.run()
